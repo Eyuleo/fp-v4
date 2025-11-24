ac4d3776b92a223dbd94949bdc9a355ff378b03a
Eyuleo <eyuleo10@gmail.com>
2025-11-24 22:20:40 +0300

    remove pending tab from order index

--- views/client/orders/index.php
+++ views/client/orders/index.php
@@ -7,9 +7,9 @@
                 <a href="/orders" class="<?php echo ! isset($_GET['status']) ? 'border-blue-500 text-blue-600' : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300' ?> whitespace-nowrap py-4 px-1 border-b-2 font-medium">
                     All Orders
                 </a>
-                <a href="/orders?status=pending" class="<?php echo isset($_GET['status']) && $_GET['status'] === 'pending' ? 'border-blue-500 text-blue-600' : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300' ?> whitespace-nowrap py-4 px-1 border-b-2 font-medium">
+                <!-- <a href="/orders?status=pending" class="<?php echo isset($_GET['status']) && $_GET['status'] === 'pending' ? 'border-blue-500 text-blue-600' : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300' ?> whitespace-nowrap py-4 px-1 border-b-2 font-medium">
                     Pending
-                </a>
+                </a> -->
                 <a href="/orders?status=in_progress" class="<?php echo isset($_GET['status']) && $_GET['status'] === 'in_progress' ? 'border-blue-500 text-blue-600' : 'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300' ?> whitespace-nowrap py-4 px-1 border-b-2 font-medium">
                     In Progress
                 </a>
